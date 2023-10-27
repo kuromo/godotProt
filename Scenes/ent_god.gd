592ad06dd562ceda7403e18bacf508c6f66e8f99
kuromo <michael.ochsner@hotmail.com>
2023-10-27 02:56:18 +0200

    use health component everywhere
    updated old enemies that used own health calculation

--- Scenes/ent_god.gd
+++ Scenes/ent_god.gd
@@ -7,10 +7,6 @@ func _ready():
 	
 func onHit(dmg):
 	hpCopmonent.damage(dmg)
-#	currHP -= dmg
-#	print(self.name+": you hit me for " + str(dmg) + " now i have:" + str(currHP) )
-#	if(currHP <= 0):
-#		onDeath()
 
 func onDeath():
 	queue_free()
